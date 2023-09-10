7322eb57bc8ac2a3a1abf8df6a762f2080c62372
LucasZielinski <77624034+LucasZielinski@users.noreply.github.com>
2023-09-10 21:06:50 +0200

    fix errors on URL

--- src/app/common/app.constants.ts
+++ src/app/common/app.constants.ts
@@ -1,5 +1,5 @@
 export class AppConstants {
   public static API_BASE_URL = "http://localhost:8080";
-  public static API_URL = AppConstants.API_BASE_URL + "api/v1/";
+  public static API_URL = AppConstants.API_BASE_URL + "/api/v1/";
   public static AUTH_API = AppConstants.API_URL + "auth/";
 }
